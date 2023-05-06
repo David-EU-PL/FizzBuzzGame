8c80a1ef883e61d80741f0e105a65a3c20d6c44e
David-EU-PL <dawidplatek.eu.pl@outlook.com>
2023-05-06 11:23:31 +0200

    Add project files.

---
+++ FizzBuzzGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FizzBuzzGame
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Witaj w prostej grze FizzBuzz. \nPodając liczbę sprawdzimy czy jest podzielna przez 3 bez reszty i/lub przez 5 bez reszty albo nie nie spełnia obu warunków. \nPodaj proszę dowolną liczbę: ");
+            var number = GetNumber();
+            
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("\nFizzBuzz");
+            }
+            else
+            {
+                if (number % 3 == 0)
+                {
+                    Console.WriteLine("\nFizz");
+                }
+                if (number % 5 == 0)
+                {
+                    Console.WriteLine("\nBuzz");
+                }
+            }
+            return number;
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłową liczbę. Możesz spróbować ponownie.\n");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
+
